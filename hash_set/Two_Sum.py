89d8568bdce8cb7861e55ffd82a540863653379a
adamsdkj <homala.asli@gmail.com>
2026-03-05 06:42:11 +0200

    two sum ex

---
+++ hash_set/Two_Sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1 ,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return None
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.twoSum([3, 4, 5, 6], 7))
