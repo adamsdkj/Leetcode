a504259cbabfd92768c1bf210bc7d6a7afd1cd6f
adamsdkj <homala.asli@gmail.com>
2026-03-07 05:14:17 +0200

    another way

--- array_hash/Group_Anagrams.py
+++ array_hash/Group_Anagrams.py
@@ -3,33 +3,47 @@ from typing import List, Dict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result, added = list(), list()
-        i, index = 0, 0
+        result = list() 
+        added = set()
+
         for i, value in enumerate(strs):
             if i in added:
                 continue
-            result.append(list())
+            group = []
             value_dict = self.fill_dict(value)
-            for j, rest_value in enumerate(strs):
+
+            for j, others in enumerate(strs):
                 if (j not in added) and (value_dict
-                   == self.fill_dict(rest_value)):
-                    result[index].append(rest_value)
-                    added.append(j)
-            index += 1
+                   == self.fill_dict(others)):
+                    group.append(others)
+                    added.add(j)
+
+            result.append(group)
         return result
 
     @staticmethod
     def fill_dict(word: str) -> Dict:
         rs = dict()
-        for chr in word:
-            if chr in rs:
-                rs[chr] += 1
-                continue
-            rs[chr] = 1
+        for c in word:
+            rs[c] = rs.get(c, 0) + 1
         return rs
+    
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for word in strs:
+            key = tuple(sorted(word))
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return list(groups.values())
 
 
 if __name__ == "__main__":
     s = Solution()
     strs = ["act", "pots", "tops", "cat", "stop", "hat"]
     print(s.groupAnagrams(strs))
+    print(s.groupAnagrams2(strs))
