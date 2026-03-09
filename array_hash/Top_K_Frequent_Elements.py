3104b3865d9288eaf9c02f0072af727320cefea8
adamsdkj <homala.asli@gmail.com>
2026-03-09 10:21:31 +0200

    finished the top k

--- array_hash/Top_K_Frequent_Elements.py
+++ array_hash/Top_K_Frequent_Elements.py
@@ -2,22 +2,37 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hash_map = dict() 
         result = dict()
-        
-        for number in nums:
-            if hash_map.get(str(number), 0) == 0:
-                hash_map[str(number)] = 1
+        for value in nums:
+            if result.get(value, 0) == 0:
+                result[value] = 1
             else:
-                hash_map[str(number)] += 1
-            if hash_map.get(str(number), 0) >= k:
-                result[str(number)] = hash_map.get(str(number), 0)        
-        result = sorted(result.items(), key=lambda x: x[1])
-        return [item[0] for item in result[::-1]]
+                result[value] += 1
+        arr = Solution.insertion_sort(list(result.items()))
+        final = list()
+        if len(arr) < k:
+            k = len(arr)
+            for _ in range(k):
+                final.append(arr[_][0])
+        else:
+            for _ in range(k):
+                final.append(arr[_][0])
+        return sorted(final)
 
+    def insertion_sort(arry: List) -> List:
+        for index in range(1, len(arry)):
+            index2 = index - 1
+            for number in arry[index2::-1]:
+                if arry[index][1] > number[1]:
+                    tmp = number
+                    arry[index2] = arry[index]
+                    arry[index] = number
+                    index = index2
+                index2 -= 1
+        return arry
 
 if __name__ == "__main__":
     s = Solution()
-    nums=[1, 2]
+    nums=[-1, -1]
     k = 2
     print(s.topKFrequent(nums, k))
